3b3038cc13be6971249a7a967381f855699cbb8c
eli <helloworld.eli@gmail.com>
2018-02-22 19:30:08 +0800

    init eCommerce

---
+++ src/products/views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.http import Http404
+
+from .models import Product
+
+
+def product_list_view(request):
+    query_set = Product.objects.all()
+    context = {
+        'object_list': query_set
+    }
+    return render(request, 'products/list.html', context)
+
+
+class ProductListView(ListView):
+    template_name = 'products/list.html'
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        return context
+
+
+def product_detail_view(request, pk=None, *args, **kwargs):
+    instance = Product.objects.get_by_id(pk)
+    if instance is None:
+        raise Http404("Product doesn't exist")
+    context = {
+        'object': instance
+    }
+    return render(request, 'products/detail.html', context)
+
+
+class ProductDetailSlugView(DetailView):
+    queryset = Product.objects.all()
+    template_name = 'products/detail.html'
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        slug = self.kwargs.get('slug')
+        try:
+            instance = Product.objects.get(slug=slug, active=True)
+        except Product.DoesNotExist:
+            raise Http404("Not found ...")
+        except Product.MultipleObjectsReturned:
+            qs = Product.objects.filter(slug=slug, active=True)
+            instance = qs.first()
+        except:
+            raise Http404('Uhhmmm')
+        return instance
+
+
+class ProductDetailView(DetailView):
+    template_name = 'products/detail.html'
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Product doesn't exist")
+        return instance
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        return context
+
+
+class ProductFeaturedListView(ListView):
+    template_name = 'products/list.html'
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return  Product.objects.all().featured()
+
+
+class ProductFeaturedDetailView(DetailView):
+    queryset = Product.objects.all().featured()
+    template_name = 'products/featured-detail.html'
