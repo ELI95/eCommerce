99957d6884c0090d69c2fca39c4971a5b962702e
eli <helloworld.eli@gmail.com>
2018-03-26 16:01:23 +0800

    how to deploy?

--- src/ecommerce/settings/base.py
+++ src/ecommerce/settings/base.py
@@ -16,7 +16,7 @@ import pymysql
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -80,7 +80,7 @@ FORCE_SESSION_TO_ONE = True
 FORCE_INACTIVE_USER_ENDSESSION= False
 
 
-MAILCHIMP_API_KEY = "b70964a00e06386839d6125b44d95d66-us12"
+MAILCHIMP_API_KEY = "00fbd8d1dd69ff4edeb9d80bd2b68ce1-us12"
 MAILCHIMP_DATA_CENTER = "us12"
 MAILCHIMP_EMAIL_LIST_ID = "7e958f5369"
 
@@ -188,3 +188,14 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', 'static_root
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', 'media_root')
+
+
+CORS_REPLACE_HTTPS_REFERER = False
+HOST_SCHEME = "http://"
+SECURE_PROXY_SSL_HEADER = None
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+SECURE_HSTS_SECONDS = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+SECURE_FRAME_DENY = False
