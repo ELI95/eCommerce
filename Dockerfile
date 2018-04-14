f08289ee298cace894cccdc9ff0bbab33167f8fd
eli <helloworld.eli@gmail.com>
2018-04-14 15:10:14 +0800

    add nginx container

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.5
 ENV PYTHONUNBUFFERED=1
-RUN mkdir /code
+RUN mkdir -p /code/log/gunicorn
 ADD .  /code/
 RUN pip install -r /code/src/requirements.txt
---
+++ src/gunicorn_config.py
@@ -0,0 +1,7 @@
+bind = "0.0.0.0:8000"
+loglevel = "INFO"
+workers = "4"
+reload = True
+
+# errorlog = "/code/log/gunicorn/error.log"
+# accesslog = "/code/log/gunicorn/access.log"
